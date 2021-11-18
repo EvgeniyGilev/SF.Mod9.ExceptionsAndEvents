92e828ab3f468cd15449d2a59936131709fc41a0
Евгений Гилев <euggil@yandex.ru>
2021-11-18 16:54:41 +0500

    задание к практикуму по модулю 9 обработка исключений, Делегаты, События

--- SF.Mod9.ExceptionsAndEvents/Program.cs
+++ SF.Mod9.ExceptionsAndEvents/Program.cs
@@ -2,11 +2,26 @@
 
 namespace SF.Mod9.ExceptionsAndEvents
 {
+    /// <summary>
+    /// Program for SF Mod 9 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main code
+        /// </summary>
+        /// <param name="args">The args.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Введено некорректное значение " + e.Message);
+                throw;
+            }
         }
     }
 }
---
+++ SF.Mod9.ExceptionsAndEvents/ReadUserNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SF.Mod9.ExceptionsAndEvents
+{
+    /// <summary>
+    /// Класс, обрабатывающий логику ввода пользователем числа в консоль
+    /// </summary>
+    public class ReadUserNumber
+    {
+        // делегат
+        public delegate void NumberEnteredHandler(int number);
+
+        // событие
+        public event NumberEnteredHandler NumberEnteredHandlerNotify;
+
+        /// <summary>
+        /// Читаем введенное число из консоли
+        /// </summary>
+        public void ReadNumber()
+        {
+            Console.WriteLine("Для сортировки списка введите 1 - сортировка по возрастанию, 2 - сортировка по убыванию");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new FormatException();
+
+        }
+    }
+}
